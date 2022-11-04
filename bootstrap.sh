20925d46ebc0b8f4f369ac4dc4facd5ab58f5dc7
Frank Dice <frank@babyvelociraptor.com>
2022-11-04 16:33:35 +0000

    Initial testing

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install the important stuff first
+apt-get update
+apt-get install -y git vim gpg wget
+
+# ssh folder and decrypting
+wget 
+
+# git clone my bootstrap repo
+
+# local symlinks
+
